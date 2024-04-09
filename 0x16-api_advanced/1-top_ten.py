daba95126e7718d86fc996a3101b4b40dda431f0
Joyce Njoroge <136052595+JoyceNjoro1@users.noreply.github.com>
2024-04-09 12:39:37 +0300

    api's

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Function to print top posts on a given Reddit subreddit."""
+import requests
+
+def top_ten(subreddit):
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(post.get("data").get("title")) for post in results.get("children")]
+
+if __name__ == "__main__":
+    subreddit = "programming"
+    top_ten(subreddit)
+
