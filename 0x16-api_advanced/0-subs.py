2a5d1811203470c70954083ec453c66b2bfecd01
Joyce Njoroge <136052595+JoyceNjoro1@users.noreply.github.com>
2024-05-07 15:59:10 +0300

    api's

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,27 +1,16 @@
 #!/usr/bin/python3
 """
-number of subscribers for a given subreddit
+Make an api call on the subreddit and return the number subscribers
 """
-
-from requests import get
+import requests
 
 
 def number_of_subscribers(subreddit):
     """
-    function that queries the Reddit API and returns the number of subscribers
-    (not active users, total subscribers) for a given subreddit.
+    how many subscribers have subscribed for a certain sub reddit
     """
-
-    if subreddit is None or not isinstance(subreddit, str):
-        return 0
-
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url, headers=user_agent)
-    results = response.json()
-
-    try:
-        return results.get('data').get('subscribers')
-
-    except Exception:
-        return 0
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    r = requests.get(url)
+    if r.status_code == 200:
+        return r.json().get("data").get("subscribers")
+    return 0
